afed564793a2bace814f611f2863e243eafe0025
Andrew Wei <andrew.wei92@yahoo.com>
2016-04-17 15:31:22 -0700

    Added redirects

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -14,6 +14,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number])
     @contact.save
+    redirect_to "/contacts/#{@contact.id}"
   end
 
   def edit
@@ -23,10 +24,12 @@ class ContactsController < ApplicationController
   def update
     @contact = Contact.find_by(id: params[:id])
     @contact.update(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number])
+    redirect_to "/contacts/#{@contact.id}"
   end
 
   def destroy
     @contact = Contact.find_by(id: params[:id])
     @contact.delete
+    redirect_to "/contacts"
   end
 end
